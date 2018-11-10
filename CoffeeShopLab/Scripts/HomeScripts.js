d0f8f1b56a32cd070c24588377960f76b384edb3
Rudy Gall <rudyegall@gmail.com>
2018-11-10 17:45:36 -0500

    Second commit

--- CoffeeShopLab/Scripts/HomeScripts.js
+++ CoffeeShopLab/Scripts/HomeScripts.js
@@ -1,7 +1,14 @@
-﻿$(function () {
+﻿function isValid(regEx, val) {
+    if (regEx.test(val)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+$(function () {
     $("#FirstName").change(function () {
         var val = $("#FirstName").val();
-    console.log(val)
         val = isValid(/^[a-zA-Z]{2,}$/, val);
         if (val) {
             $("#FirstNameValid").show();
@@ -11,12 +18,78 @@
             $("#FirstNameInvalid").show();
             $("#FirstNameValid").hide();
         }
-
-    })
-    
+    })  
 })
-
-
+$(function () {
+    $("#LastName").change(function () {
+        var val = $("#LastName").val();
+        val = isValid(/^[a-zA-Z]{2,}$/, val);
+        if (val) {
+            $("#LastNameValid").show();
+            $("#LastNameInvalid").hide();
+        }
+        else {
+            $("#LastNameInvalid").show();
+            $("#LastNameValid").hide();
+        }
+    })
+})
+$(function () {
+    $("#Email").change(function () {
+        var val = $("#Email").val();
+        val = isValid(/^[A-z0-9]{5,30}(@)(.+){5,10}$/, val);
+        if (val) {
+            $("#EmailValid").show();
+            $("#EmailInvalid").hide();
+        }
+        else {
+            $("#EmailInvalid").show();
+            $("#EmailValid").hide();
+        }
+    })
+})
+$(function () {
+    $("#PhoneNumber").change(function () {
+        var val = $("#PhoneNumber").val();
+        val = isValid(/^\d{10}$/, val);
+        if (val) {
+            $("#PhoneNumberValid").show();
+            $("#PhoneNumberInvalid").hide();
+        }
+        else {
+            $("#PhoneNumberInvalid").show();
+            $("#PhoneNumberValid").hide();
+        }
+    })
+})
+$(function () {
+    $("#PassWord").change(function () {
+        var val = $("#PassWord").val();
+        val = isValid(/^[a-zA-Z]\w{3,14}$/, val);
+        if (val) {
+            $("#PassWordValid").show();
+            $("#PassWordInvalid").hide();
+        }
+        else {
+            $("#PassWordInvalid").show();
+            $("#PassWordValid").hide();
+        }
+    })
+})
+$(function () {
+    $("#Date").change(function () {
+        var val = $("#Date").val();
+        val = isValid(/^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$/, val);
+        if (val) {
+            $("#DateValid").show();
+            $("#DateInvalid").hide();
+        }
+        else {
+            $("#DateInvalid").show();
+            $("#DateValid").hide();
+        }
+    })
+})
 function ValidateAge() {
     var agePattern = /^[1-9]$|^[1-9][0-9]$|^(100)$/;
      
@@ -29,24 +102,18 @@ function ValidateAge() {
         document.getElementById("AgeInvalid").style.display = "block";
     }
 }
-function isValid(regEx, val) {
+function ValidateSSN() {
+    var ssnPattern = /^\d{3}-?\d{2}-?\d{4}$/;
 
-    if (regEx.test(val)) {
-        return true;
+    if (ssnPattern.test(document.getElementById("SSN").value)) {
+        document.getElementById("SSNValid").style.display = "block";
+        document.getElementById("SSNInvalid").innerHTML = "none";
     }
     else {
-        return false;
+        document.getElementById("SSNValid").style.display = "none";
+        document.getElementById("SSNInvalid").innerHTML = "block";
     }
-
-
-
 }
-
-
-
-
-
-
 
 
 
